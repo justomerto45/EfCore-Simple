d299ab92e74bae7b1af7fa9448db8a501cb287ee
mertt <mertt@457MC>
2023-06-28 15:39:59 +0200

    Projektdateien hinzufügen.

---
+++ EfCore_Easy/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using EfCore_Easy;
+using Microsoft.EntityFrameworkCore;
+
+public class MyDbContext : DbContext
+{
+    public DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MyDatabase;Trusted_Connection=True;");
+        }
+    }
+
+}
---
+++ EfCore_Easy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        using (var context = new MyDbContext())
+        {
+            // Rufe die Daten aus der Datenbank ab und gib sie aus
+            var people = context.People.ToList();
+            foreach (var person in people)
+            {
+                Console.WriteLine($"ID: {person.Id}, Name: {person.Name}");
+            }
+        }
+    }
+}
